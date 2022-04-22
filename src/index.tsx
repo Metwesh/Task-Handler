ba6b29e35218bff863de9feee98f19776c14acc3
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-04-22 21:04:55 +0200

    Fixed basename

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/task-handler">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
